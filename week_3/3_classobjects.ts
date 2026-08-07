133dfe27e9c44d8e8140d4fcf7e65f3a36878af8
Vanteddu Lakshmi Hari Sai Devi <hasithasnehitha@gmail.com>
2026-08-07 08:59:17 +0530

    Update 3_classobjects.ts

---
+++ week_3/3_classobjects.ts
@@ -0,0 +1,46 @@
+class Employee {
+    name: string;
+    designation: string;
+    salary: number;
+
+    
+    constructor(name: string);
+    constructor(name: string, designation: string, salary: number);
+
+   
+    constructor(name: string, designation?: string, salary?: number) {
+        this.name = name;
+        this.designation = designation ?? "Trainee";
+        this.salary = salary ?? 25000;
+    }
+
+    display(): void {
+        console.log("Name:", this.name);
+        console.log("Designation:", this.designation);
+        console.log("Salary:", this.salary);
+        console.log("----------------------");
+    }
+}
+
+
+
+const emp1 = new Employee("Lahari");
+const emp2 = new Employee("Sai", "Software Engineer", 60000);
+
+
+
+emp1.display();
+emp2.display();
+
+
+console.log(emp1.name);
+console.log(emp2.salary);
+
+
+emp1.designation = "Junior Developer";
+emp1.salary = 35000;
+
+
+console.log("After Updating");
+
+emp1.display();
